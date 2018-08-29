e34005e881695262cfa9d7eeb879afa457ce79d5
SunnyMarkLiu <sunnymarkliu101@gmail.com>
2018-08-29 21:35:42 +0800

    add some problems

---
+++ 二叉搜索树的后序遍历序列/Solution.java
@@ -0,0 +1,48 @@
+package 二叉搜索树的后序遍历序列;
+
+/**
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
+ * 如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+ */
+public class Solution {
+
+    /**
+     * 二叉搜索树的后序遍历的最后一个值为根节点，
+     * 去掉根节点之后的序列继续判断是否满足 => 递归
+     */
+    public boolean VerifySquenceOfBST(int[] sequence) {
+        if (sequence == null || sequence.length == 0)
+            return false;
+
+        return helper(sequence, 0, sequence.length - 1);
+    }
+
+    /**
+     * 递归判断序列是否满足 BST 的后序遍历
+     */
+    private boolean helper(int[] seq, int start, int end) {
+        if (start > end)
+            return true;
+
+        int root = seq[end];
+        // 找到左子树的位置，左子树的值都比 root 小
+        int left = start;
+        while (left <= end - 1) {
+            if (seq[left] > root)   // 此时出现大于root的值，属于右子树
+                break;
+            left++;
+        }
+        // 判断此时的右子树是否满足都大于 root
+        int right = left;
+        while (right <= end - 1) {
+            if (seq[right] < root)  // 不满足，false
+                return false;
+            right++;
+        }
+        // 递归判断 root 前面的序列是否满足
+        // return helper(seq, start, end - 1);
+
+        // 此处可优化为判断左右子树是否满足
+        return helper(seq, start, left - 1) && helper(seq, left, end - 1);
+    }
+}
---
+++ 复杂链表的复制/Solution.java
@@ -0,0 +1,55 @@
+package 复杂链表的复制;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，
+ * 另一个特殊指针指向任意一个节点），返回结果为复制后复杂链表的head。
+ * （注意，输出结果中请不要返回参数中的节点引用，否则判题程序会直接返回空）
+ */
+public class Solution {
+
+    public class RandomListNode {
+        int label;
+        RandomListNode next = null;
+        RandomListNode random = null;
+
+        RandomListNode(int label) {
+            this.label = label;
+        }
+    }
+
+    /**
+     * 复制的时候用 Map 保存原始 node 和新复制 node 的映射关系
+     */
+    public RandomListNode Clone(RandomListNode pHead) {
+        if (pHead == null)
+            return null;
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
+
+        RandomListNode newHead = new RandomListNode(pHead.label);
+        map.put(pHead, newHead);
+
+        RandomListNode oldTmp = pHead.next;
+        RandomListNode newTmp = newHead;
+
+        while (oldTmp != null) {
+            RandomListNode newNode = new RandomListNode(oldTmp.label);
+            map.put(oldTmp, newNode);
+            newTmp.next = newNode;
+            newTmp = newTmp.next;
+            oldTmp = oldTmp.next;
+        }
+
+        // 更新 random 指针
+        oldTmp = pHead;
+        newTmp = newHead;
+        while (oldTmp != null) {
+            newTmp.random = map.get(oldTmp.random);
+            newTmp = newTmp.next;
+            oldTmp = oldTmp.next;
+        }
+        return newHead;
+    }
+}
